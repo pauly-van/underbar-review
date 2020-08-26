6c2fd162e074ef7582b55d5f0601068310bb5bef
Paul Nguyen <paulngu1120@gmail.com>
2020-08-26 15:04:50 +0000

    Testing eslint and first commit

--- src/underbar.js
+++ src/underbar.js
@@ -9,6 +9,7 @@
   _.identity = function(val) {
   };
 
+
   /**
    * COLLECTIONS
    * ===========
@@ -30,13 +31,14 @@
 
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
-  _.first = function(array, n) {
-    return n === undefined ? array[0] : array.slice(0, n);
+  _.first=function(array, n) {
+    return n===undefined ? array[0] : array.slice(0, n);
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
+
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -107,19 +109,19 @@
   // Reduces an array or object to a single value by repetitively calling
   // iterator(accumulator, item) for each item. accumulator should be
   // the return value of the previous iterator call.
-  //  
+  //
   // You can pass in a starting value for the accumulator as the third argument
   // to reduce. If no starting value is passed, the first element is used as
   // the accumulator, and is never passed to the iterator. In other words, in
   // the case where a starting value is not passed, the iterator is not invoked
   // until the second element, with the first element as its second argument.
-  //  
+  //
   // Example:
   //   var numbers = [1,2,3];
   //   var sum = _.reduce(numbers, function(total, number){
   //     return total + number;
   //   }, 0); // should be 6
-  //  
+  //
   //   var identity = _.reduce([5], function(total, number){
   //     return total + number * number;
   //   }); // should be 5, regardless of the iterator function passed in
