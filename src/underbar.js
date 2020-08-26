2ea680f27f48f27265ba007cad67cd492a8c2247
Paul Nguyen <paulngu1120@gmail.com>
2020-08-26 18:00:07 +0000

    First pair session completion to reduce

--- src/underbar.js
+++ src/underbar.js
@@ -7,6 +7,7 @@
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
   _.identity = function(val) {
+    return val;
   };
 
 
@@ -31,14 +32,19 @@
 
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
-  _.first=function(array, n) {
-    return n===undefined ? array[0] : array.slice(0, n);
+  _.first = function(array, n) {
+    return n === undefined ? array[0] : array.slice(0, n);
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
-
+    if(n === 0){
+      return [];
+    }else if(n>array.length){
+      return array;
+    }
+    return n === undefined ? array[array.length-1] : array.slice(n-1);
   };
 
   // Call iterator(value, key, collection) for each element of collection.
@@ -47,6 +53,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(let i=0;i<collection.length;i++){
+       iterator(collection[i], i, collection);
+      }
+    } 
+    else {
+      for(let key in collection){
+        iterator(collection[key], key, collection);
+      }
+    }
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
@@ -68,24 +84,68 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    // create array container
+    let returnArr = [];
+    // use each function for collection
+    _.each(collection, function(elem){
+      if(test(elem)){
+       returnArr.push(elem); 
+      }
+    })
+      // invoke test to each element
+      // push result into array container
+    // return array container
+    return returnArr;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
-    // TIP: see if you can re-use _.filter() here, without simply
-    // copying code in and modifying it
+    // call on filter
+    return _.filter(collection, function(elem){
+      return !test(elem);
+    });
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+    // create uniqArr
+    let uniqArr = [];
+    // create uniqObj
+    let uniqObj = {};
+    // use each to iterate through array collection
+    iterator = iterator || _.identity;
+
+    _.each(array, function(element) {
+      var transformed = iterator(element);
+      if (uniqObj[transformed] === undefined) {
+        uniqObj[transformed] = element;
+      }
+    });
+
+    // use each on uniqObj
+    _.each(uniqObj, function(value) {
+      // push to key to uniqArr
+      uniqArr.push(value);
+    });
+    
+    //return uniqArr
+    return uniqArr;
   };
 
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
+    let result = [];
+
+    _.each(collection, function(elem){
+      result.push(iterator(elem))
+    })
+
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+
+    return result;
   };
 
   /*
@@ -127,6 +187,15 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    // if accumulator is undefined
+      // then first element of collection is accumulator
+      // create variable noAccu
+    // use each to iterate through collection
+      // if noAcc is true
+        // try to continue or increment
+        // set noAcc to false
+      
+      
   };
 
   // Determine if the array or object contains a given value (using `===`).
